855f94d1aab7b73b52a6c2eb102cfa3d88491206
PhucDuong-SSS <phucdndev@gmail.com>
2022-09-14 20:29:25 +0700

    add redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
 import Home from "./components/Home";
 import NotFound from "./components/NotFound";
+import productsReducer from "./features/productsSlice";
+
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  },
+});
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <div className="content-container">
-          <Switch>
-            <Route path="/cart" component={Cart} />
-            <Route path="/not-found" component={NotFound} />
-            <Route path="/" exact component={Home} />
-            <Redirect to="/not-found" />
-          </Switch>
+      <Provider store={store}>
+        <div className="App">
+          <Navbar />
+          <div className="content-container">
+            <Switch>
+              <Route path="/cart" component={Cart} />
+              <Route path="/not-found" component={NotFound} />
+              <Route path="/" exact component={Home} />
+              <Redirect to="/not-found" />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Provider>
     </BrowserRouter>
   );
 }
